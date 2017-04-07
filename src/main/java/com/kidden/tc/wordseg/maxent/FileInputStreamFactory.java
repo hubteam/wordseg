6acf161c342309dbffe011292120617e75b0495f
kidden <kiddenliu@sina.com>
2017-04-07 14:38:18 +0800

    add FileInputStreamFactory.

---
+++ src/main/java/com/kidden/tc/wordseg/maxent/FileInputStreamFactory.java
@@ -0,0 +1,26 @@
+package com.kidden.tc.wordseg.maxent;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import opennlp.tools.util.InputStreamFactory;
+
+
+public class FileInputStreamFactory implements InputStreamFactory {
+
+  private File file;
+
+  public FileInputStreamFactory(File file) throws FileNotFoundException {
+    if(!file.exists()) {
+      throw new FileNotFoundException("File '" + file + "' cannot be found");
+    }
+    this.file = file;
+  }
+
+  @Override
+  public InputStream createInputStream() throws IOException {
+    return new FileInputStream(file);
+  }
+}
