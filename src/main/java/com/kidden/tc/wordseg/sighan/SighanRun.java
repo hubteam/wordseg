369350ed66dc67248e9c291047d92b98b1567893
kidden <kiddenliu@sina.com>
2017-05-10 13:38:19 +0800

    Add cross validator CLI.

--- src/main/java/com/kidden/tc/wordseg/sighan/SighanRun.java
+++ src/main/java/com/kidden/tc/wordseg/sighan/SighanRun.java
@@ -1,21 +1,22 @@
 package com.kidden.tc.wordseg.sighan;
 
+import com.kidden.tc.wordseg.maxent.DefaultWordSegContextGenerator;
 import com.kidden.tc.wordseg.maxent.FileInputStreamFactory;
 import com.kidden.tc.wordseg.maxent.WordSegContextGenerator;
+import com.kidden.tc.wordseg.maxent.WordSegContextGeneratorClosed;
 import com.kidden.tc.wordseg.maxent.WordSegContextGeneratorConf;
+import com.kidden.tc.wordseg.maxent.WordSegCrossValidator;
+import com.kidden.tc.wordseg.maxent.WordSegErrorPrinter;
 import com.kidden.tc.wordseg.maxent.WordSegEvaluator;
 import com.kidden.tc.wordseg.maxent.WordSegMeasure;
 import com.kidden.tc.wordseg.maxent.WordSegModel;
 import com.kidden.tc.wordseg.maxent.WordSegSample;
 import com.kidden.tc.wordseg.maxent.WordSegmenterME;
 import com.kidden.tc.wordseg.maxent.WordTagSampleStream;
-import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.HashSet;
 import java.util.Properties;
 import opennlp.tools.util.ObjectStream;
@@ -29,13 +30,14 @@ import opennlp.tools.util.TrainingParameters;
  */
 public class SighanRun {
 
-    private static class Corpus {
+    public static class Corpus {
 
         public String name;
         public String encoding;
         public String trainFile;
         public String goldFile;
         public String modelFile;
+        public String errorFile;
     }
 
     private static String[] corpusName = {"pku", "msr", "cityu", "as"};
@@ -54,7 +56,7 @@ public class SighanRun {
 //        String testFile = config.getProperty(corpus + "." + "corpus.test.file");
             String goldFile = config.getProperty(name + "." + "corpus.gold.file");
             String modelFile = config.getProperty(name + "." + "corpus.model.file");
-//        String errorFile = config.getProperty(corpus + "." + "corpus.error.file");
+            String errorFile = config.getProperty(name + "." + "corpus.error.file");
 //        String resultFile = config.getProperty(corpus + "." + "corpus.test.result.file");
             Corpus corpus = new Corpus();
             corpus.name = name;
@@ -62,11 +64,52 @@ public class SighanRun {
             corpus.trainFile = corpusFile;
             corpus.goldFile = goldFile;
             corpus.modelFile = modelFile;
+            corpus.errorFile = errorFile;
 
             corpora[i] = corpus;
         }
 
         return corpora;
+    }
+
+    public static Corpus[] getCorporaFromConf() throws IOException {
+        Properties config = new Properties();
+        InputStream configStream = SighanRun.class.getClassLoader().getResourceAsStream("com/kidden/tc/wordseg/sighan/corpus.properties");
+        config.load(configStream);
+
+        Corpus[] corpora = new Corpus[corpusName.length];
+        for (int i = 0; i < corpusName.length; i++) {
+            String name = corpusName[i];
+            String encoding = config.getProperty(name + "." + "corpus.encoding");
+            String corpusFile = config.getProperty(name + "." + "corpus.train.file");
+//        String dictFile = config.getProperty(corpus + "." + "corpus.dict.file");
+//        String testFile = config.getProperty(corpus + "." + "corpus.test.file");
+            String goldFile = config.getProperty(name + "." + "corpus.gold.file");
+            String modelFile = config.getProperty(name + "." + "corpus.model.file");
+            String errorFile = config.getProperty(name + "." + "corpus.error.file");
+//        String resultFile = config.getProperty(corpus + "." + "corpus.test.result.file");
+            Corpus corpus = new Corpus();
+            corpus.name = name;
+            corpus.encoding = encoding;
+            corpus.trainFile = corpusFile;
+            corpus.goldFile = goldFile;
+            corpus.modelFile = modelFile;
+            corpus.errorFile = errorFile;
+
+            corpora[i] = corpus;
+        }
+
+        return corpora;
+    }
+
+    public static Corpus getCorpus(Corpus[] corpora, String name) {
+        for (Corpus c : corpora) {
+            if (c.name.equalsIgnoreCase(name)) {
+                return c;
+            }
+        }
+
+        return null;
     }
 
     /**
@@ -76,16 +119,24 @@ public class SighanRun {
      */
     public static void runFeatureOnCorpus(WordSegContextGenerator contextGenerator, Corpus corpus, TrainingParameters params) throws IOException {
         System.out.println("ContextGenerator: " + contextGenerator);
-        System.out.println("Training on " + corpus.name);
+        System.out.println("Training on " + corpus.name + "...");
         WordSegModel model = WordSegmenterME.train(new File(corpus.trainFile), params, contextGenerator, corpus.encoding);
 
-        System.out.println("Building dictionary on " + corpus.name);
+        System.out.println("Building dictionary on " + corpus.name + "...");
         HashSet<String> dict = WordSegmenterME.buildDictionary(new File(corpus.trainFile), corpus.encoding);
 
-        System.out.println("Evaluating on" + corpus.name);
+        System.out.println("Evaluating on " + corpus.name + "...");
         WordSegmenterME tagger = new WordSegmenterME(model, contextGenerator);
         WordSegMeasure measure = new WordSegMeasure(dict);
-        WordSegEvaluator evaluator = new WordSegEvaluator(tagger);
+        WordSegErrorPrinter errorMonitor = null;
+        WordSegEvaluator evaluator = null;
+        if (corpus.errorFile != null) {
+            System.out.println("Print error to file " + corpus.errorFile);
+            errorMonitor = new WordSegErrorPrinter(new FileOutputStream(corpus.errorFile));
+            evaluator = new WordSegEvaluator(tagger, errorMonitor);
+        } else {
+            evaluator = new WordSegEvaluator(tagger);
+        }
         evaluator.setMeasure(measure);
         ObjectStream<String> lineStream = new PlainTextByLineStream(new FileInputStreamFactory(new File(corpus.goldFile)), corpus.encoding);
         ObjectStream<WordSegSample> sampleStream = new WordTagSampleStream(lineStream);
@@ -104,23 +155,75 @@ public class SighanRun {
         for (Corpus corpus : corpora) {
             runFeatureOnCorpus(contextGenerator, corpus, params);
         }
-
     }
 
-    public static void main(String[] args) throws IOException {
-        TrainingParameters params = TrainingParameters.defaultParams();
-        params.put(TrainingParameters.CUTOFF_PARAM, Integer.toString(3));
-        
+    private static WordSegContextGenerator getContextGenerator(Properties conf) throws IOException {
+//        Properties featureConf = new Properties();
+//        InputStream featureStream = SighanRun.class.getClassLoader().getResourceAsStream("com/kidden/tc/wordseg/maxent/sighan/feature.properties");
+//        featureConf.load(featureStream);
+//        WordSegContextGeneratorConf contextGen = new WordSegContextGeneratorConf(featureConf);
+
+        String featureClass = conf.getProperty("feature.class");
+        if(featureClass.equals("com.kidden.tc.wordseg.maxent.WordSegContextGeneratorConf"))
+            return  new WordSegContextGeneratorConf(conf);
+        else
+            return new WordSegContextGeneratorClosed();
+    }
+
+    /**
+     * Cross validate on given corpus
+     *
+     * @param corpusName
+     * @throws IOException
+     */
+    private static void crossValidate(String corpusName) throws IOException {
         Properties config = new Properties();
         InputStream configStream = SighanRun.class.getClassLoader().getResourceAsStream("com/kidden/tc/wordseg/maxent/sighan/corpus.properties");
         config.load(configStream);
         Corpus[] corpora = getCorporaFromConf(config);
-        
-        Properties featureConf = new Properties();
-        InputStream featureStream = SighanRun.class.getClassLoader().getResourceAsStream("com/kidden/tc/wordseg/maxent/sighan/feature.properties");
-        featureConf.load(featureStream);
-        WordSegContextGeneratorConf contextGen = new WordSegContextGeneratorConf(featureConf);
-        
+        Corpus corpus = getCorpus(corpora, corpusName);
+
+        WordSegContextGenerator contextGen = getContextGenerator(config);
+
+        ObjectStream<String> lineStream = new PlainTextByLineStream(new FileInputStreamFactory(new File(corpus.trainFile)), corpus.encoding);
+        ObjectStream<WordSegSample> sampleStream = new WordTagSampleStream(lineStream);
+
+        TrainingParameters params = TrainingParameters.defaultParams();
+        params.put(TrainingParameters.CUTOFF_PARAM, Integer.toString(3));
+
+        WordSegCrossValidator crossValidator = new WordSegCrossValidator("zh", params);
+
+        System.out.println(contextGen);
+        crossValidator.evaluate(sampleStream, 10, contextGen);
+    }
+
+    public static void main(String[] args) throws IOException {
+        String cmd = args[0];
+        if (cmd.equals("-run")) {
+            runFeature();
+        } else if (cmd.equals("-cross")) {
+            String corpus = args[1];
+
+            crossValidate(corpus);
+        }
+    }
+
+    /**
+     * Traing and test on 4 corpors using one feature set
+     *
+     * @throws IOException
+     */
+    private static void runFeature() throws IOException {
+        TrainingParameters params = TrainingParameters.defaultParams();
+        params.put(TrainingParameters.CUTOFF_PARAM, Integer.toString(3));
+
+        Properties config = new Properties();
+        InputStream configStream = SighanRun.class.getClassLoader().getResourceAsStream("com/kidden/tc/wordseg/maxent/sighan/corpus.properties");
+        config.load(configStream);
+        Corpus[] corpora = getCorporaFromConf(config);
+
+        WordSegContextGenerator contextGen = getContextGenerator(config);
+
         runFeatureOnCorpora(contextGen, corpora, params);
     }
 }
